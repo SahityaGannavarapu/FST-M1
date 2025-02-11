8e52e2f1744d315d9af5b16738f00c171e55eb58
SahityaGannavarapu <Gannavarapu.Sahitya@ibm.com>
2025-02-11 17:51:17 +0530

    Update car.java

--- Java/Activities/car.java
+++ Java/Activities/car.java
@@ -1,24 +1,37 @@
 package activities;
 
 public class Car {
-    String sColor;
-    String sTransmission;
-    String sMake;
-    int iDoors;
-    int iTyres;
-//constructor
-    Car(){
-        iDoors = 4;
-        iTyres = 4;
-    }
-//Methods
-    public void displayCharacteristics(){
- System.out.println ("Color of the car :" + sColor);
- System.out.println("Make of the Car :" + sMake);
- System.out.println("No. of Doors of the Car :" + iDoors);
- System.out.println("No.of Tyres of the car :" + iTyres);
- System.out.println("Transmission of the Car :" + sTransmission);
-    }
+	String color;
+	String transmission;
+	int make;
+	int tyres;
+	int doors;
+	
+	Car(String color, String transmission, int make)
+	{
+		this.color = color;
+		this.transmission = transmission;
+		this.make = make;
+		this.tyres = 4;
+		this.doors = 4;
+	}
+	public void displayCharacteristics()
+	{
+		System.out.println("Color: " + color);
+		System.out.println("Transmission tyep: " + transmission);
+		System.out.println("Year of making: " + make);
+		System.out.println("No. of tyres: " + tyres);
+		System.out.println("No. of doors: " + doors);	
+	}
+	public void accelarate()
+	{
+		System.out.println("Car is moving forward");
+	}
+	public void brake()
+	{
+		System.out.println("Car is stopped");
+	}
+}
 public  void accelerate(){
         System.out.println("Car is moving forward");
 }
